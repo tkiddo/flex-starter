9b6cd997b9e404bdf04420680a2627ef1f139500
tkiddo <justforgit@163.com>
2022-10-10 13:27:33 +0800

    feat: add element

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import "./App.css";
-import ViewBox from "./components/ViewBox";
-import styled from "styled-components";
-import { Form, Radio } from "antd";
-import { useState } from "react";
+import './App.css'
+import ViewBox from './components/ViewBox'
+import styled from 'styled-components'
+import { Button, Form, Radio, Space } from 'antd'
+import { useState } from 'react'
 
 const Main = styled.main`
   display: flex;
   width: 100vw;
   height: 100vh;
-`;
+`
 
 const Left = styled.section`
   width: 40%;
@@ -18,7 +18,7 @@ const Left = styled.section`
     flex-direction: column;
     gap: 10px;
   }
-`;
+`
 
 const Right = styled.section`
   width: 60%;
@@ -27,75 +27,80 @@ const Right = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`;
+`
 
-const containerOptions = [
+const parentOptions = [
   {
-    cssName: "flex-direction",
-    name: "flexDirection",
-    values: ["row", "row-reverse", "column", "column-reverse"],
-    defaultValue: "row",
+    cssName: 'flex-direction',
+    name: 'flexDirection',
+    values: ['row', 'row-reverse', 'column', 'column-reverse'],
+    defaultValue: 'row',
   },
   {
-    cssName: "flex-wrap",
-    name: "flexWrap",
-    values: ["nowrap", "wrap", "wrap-reverse"],
-    defaultValue: "nowrap",
+    cssName: 'flex-wrap',
+    name: 'flexWrap',
+    values: ['nowrap', 'wrap', 'wrap-reverse'],
+    defaultValue: 'nowrap',
   },
   {
-    cssName: "justify-content",
-    name: "justifyContent",
+    cssName: 'justify-content',
+    name: 'justifyContent',
     values: [
-      "flex-start",
-      "flex-end",
-      "center",
-      "space-between",
-      "space-around",
-      "space-evenly",
+      'flex-start',
+      'flex-end',
+      'center',
+      'space-between',
+      'space-around',
+      'space-evenly',
     ],
-    defaultValue: "flex-start",
+    defaultValue: 'flex-start',
   },
   {
-    cssName: "align-items",
-    name: "alignItems",
-    values: ["flex-start", "flex-end", "center", "stretch", "baseline"],
-    defaultValue: "flex-start",
+    cssName: 'align-items',
+    name: 'alignItems',
+    values: ['flex-start', 'flex-end', 'center', 'stretch', 'baseline'],
+    defaultValue: 'flex-start',
   },
   {
-    cssName: "align-content",
-    name: "alignContent",
+    cssName: 'align-content',
+    name: 'alignContent',
     values: [
-      "flex-start",
-      "flex-end",
-      "center",
-      "stretch",
-      "space-between",
-      "space-around",
+      'flex-start',
+      'flex-end',
+      'center',
+      'stretch',
+      'space-between',
+      'space-around',
     ],
-    defaultValue: "flex-start",
+    defaultValue: 'flex-start',
   },
-];
+]
 
 function App() {
-  const [parentStyle, setParentStyle] = useState({});
-  const [form] = Form.useForm();
+  const [parentStyle, setParentStyle] = useState({})
+  const [childrenLength, setChildrenLength] = useState(5)
+  const [form] = Form.useForm()
   const parentStyleChange = (changedFields, allFields) => {
-    const newStyle = {};
+    const newStyle = {}
     allFields.forEach((field) => {
-      newStyle[field.name[0]] = field.value;
-    });
-    setParentStyle(newStyle);
-  };
+      newStyle[field.name[0]] = field.value
+    })
+    setParentStyle(newStyle)
+  }
+  const add = () => {
+    setChildrenLength(childrenLength + 1)
+  }
   return (
     <Main>
       <Left>
+        <div>容器元素：</div>
         <Form
           className="options"
           layout="vertical"
           form={form}
           onFieldsChange={parentStyleChange}
         >
-          {containerOptions.map(
+          {parentOptions.map(
             ({ cssName, name, values, defaultValue }, index) => {
               return (
                 <Form.Item
@@ -112,16 +117,20 @@ function App() {
                     ))}
                   </Radio.Group>
                 </Form.Item>
-              );
+              )
             }
           )}
         </Form>
+        <Space>
+          <div>项目元素：</div>
+          <Button type="primary" onClick={add}>添加项目</Button>
+        </Space>
       </Left>
       <Right>
-        <ViewBox parentStyle={parentStyle} />
+        <ViewBox parentStyle={parentStyle} childrenLength={childrenLength}/>
       </Right>
     </Main>
-  );
+  )
 }
 
-export default App;
+export default App
